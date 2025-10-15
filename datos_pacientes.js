f63e5fc9843aee7fde870181469b14eb319dfde7
EKDNNZ <erik.dannenberg@alumnat.ins-mediterrania.cat>
2025-10-15 17:56:37 +0200

    Add files via upload

---
+++ datos_pacientes.js
@@ -0,0 +1,44 @@
+const datosPacientes = {
+    'P001': {
+        nombre: 'Hombre de 59 años con cervicalgia crónica.',
+        observaciones: 'No presenta observaciones adicionales.',
+        recomendacion: '40 Hz + 432 Hz durante 30 minutos, preferiblemente por la mañana.',
+        objetivo: 'Aliviar el dolor nervioso y reducir el estrés.'
+    },
+    'P002': {
+        nombre: 'Hombre de 56 años con dolor neuropático.',
+        observaciones: 'Presenta depresión y en alguna ocasión ha necesitado tomar opioides.',
+        recomendacion: '40 Hz + 9 Hz durante 40 minutos, por la mañana o por la tarde.',
+        objetivo: 'Liberación emocional, mejora del estado de ánimo y alivio del dolor.'
+    },
+    'P003': {
+        nombre: 'Mujer de 38 años con ciática.',
+        observaciones: 'No presenta observaciones adicionales.',
+        recomendacion: '40 Hz + 174 Hz durante 30 minutos, preferentemente por la tarde.',
+        objetivo: 'Alivio del dolor neuropático y relajación muscular.'
+    },
+    'P004': {
+        nombre: 'Hombre de 61 años con lumbalgia crónica.',
+        observaciones: 'No hay observaciones adicionales.',
+        recomendacion: '432 Hz + 852 Hz durante 30 minutos, por la tarde o la noche.',
+        objetivo: 'Alivio del dolor lumbar y reducción de la ansiedad.'
+    },
+    'P005': {
+        nombre: 'Mujer de 42 años con lumbalgia crónica.',
+        observaciones: 'No hay observaciones adicionales.',
+        recomendacion: '432 Hz + 852 Hz durante 30 minutos, por la tarde o la noche.',
+        objetivo: 'Alivio del dolor lumbar y reducción de la ansiedad.'
+    },
+    'P006': {
+        nombre: 'Mujer de 50 años con fibromialgia y depresión.',
+        observaciones: '',
+        recomendacion: '40 Hz + 528 Hz durante 40 minutos, por la mañana.',
+        objetivo: 'Reducir el dolor generalizado, favorecer la liberación emocional y aumentar la vitalidad.'
+    },
+    'P007': {
+        nombre: 'Hombre de 45 años con migraña crónica y depresión.',
+        observaciones: '',
+        recomendacion: '40 Hz + 432 Hz durante 30 minutos, por la mañana.',
+        objetivo: 'Reducir la intensidad de la migraña y mejorar el estado de ánimo.'
+    }
+};
